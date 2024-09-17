1f9acb7bfb875eb1a7cde8761aee7ee77d96258d
Joao Sanchez <sanchezaej@gmail.com>
2024-09-17 03:33:42 -0500

    docs: se agrega comentarios en la clase inicial application

--- src/main/java/com/ejsa/academy/AcademyApplication.java
+++ src/main/java/com/ejsa/academy/AcademyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AcademyApplication {
 
+	//Se agrega este comentario para simular un commit de documentacion.
 	public static void main(String[] args) {
 		SpringApplication.run(AcademyApplication.class, args);
 	}
